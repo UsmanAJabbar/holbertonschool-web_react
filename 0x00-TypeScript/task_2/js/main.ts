5ed98cc465a473feb59f96289e201cc084feca9f
UsmanGTA <hi@usmanjabbar.com>
2021-06-21 11:08:32 +0000

    Added initial commit for task 6

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -21,7 +21,12 @@ class Teacher implements TeacherInterface {
 }
 
 const createEmployee = (salary: number | string): Teacher | Director => {
-  if (typeof salary == 'string' && parseInt(salary) == NaN) salary = salary.slice(1);
-  if (salary < 500) return new Teacher();
+  if ((typeof salary == 'string' && ( parseInt(salary) < 500 || parseInt(salary.slice(1)) < 500)) ||
+       typeof salary == 'number' && salary < 500) return new Teacher();
   else return new Director();
+}
+const isDirector = (employee: Teacher | Director): employee is Director => employee instanceof Director;
+const executeWork = (employee: Teacher | Director): string => {
+  if (isDirector(employee)) return employee.workDirectorTasks();
+  return employee.workTeacherTasks()
 }
