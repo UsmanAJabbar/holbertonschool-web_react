e74c825a752b4fec983e2c64e167fbbc4fd00a33
UsmanGTA <hi@usmanjabbar.com>
2021-06-21 09:48:52 +0000

    Added initial commit for task 4

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,39 @@
+interface Teacher {
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [extraObj: string]: any,
+};
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+/* Interface defines a singature param so functions that associated with the interface would be accepted */
+interface printTeacherFunction { (firstName: string, lastName: string): string }
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => `${firstName[0]}. ${lastName}`;
+
+interface studentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+interface studentClassConstructor {
+  new (firstName: string, lastName: string): StudentClass,
+}
+class StudentClass implements studentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
