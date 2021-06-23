34b61f980f4e9a057fa527981afdced328c535e2
UsmanGTA <hi@usmanjabbar.com>
2021-06-23 21:27:21 +0000

    pushed new fixes

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 $('body').append(
-  $('<p>').text('Holberton Dashboard'),
-  $('<p>').text('Dashboard data for the students'),
-  $('<p>').text('Copyright - Holberton School'),
+  $('<p></p>').text('Holberton Dashboard'),
+  $('<p></p>').text('Dashboard data for the students'),
+  $('<p></p>').text('Copyright - Holberton School'),
 );
--- 0x01-Webpack/task_1/src/dashboard_main.js
+++ 0x01-Webpack/task_1/src/dashboard_main.js
@@ -1,13 +1,11 @@
 import $ from 'jquery';
 
-$(document).ready(
-  $('body').append(
-    $('<p>').text('Holberton Dashboard'),
-    $('<p>').text('Dashboard data for the students'),
-    $('<button>').text('Click here to get started'),
-    $('<p>').attr('id', 'count'),
-    $('<p>').text('Copyright - Holberton School'),
-  )
+$('body').append(
+  $('<p></p>').text('Holberton Dashboard'),
+  $('<p></p>').text('Dashboard data for the students'),
+  $('<button>').text('Click here to get started'),
+  $('<p></p>').attr('id', 'count'),
+  $('<p></p>').text('Copyright - Holberton School'),
 )
 
 function updateCounter() {
