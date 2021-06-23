944029c8e9cf7c34bb3e8be245e6857b91969fbb
UsmanGTA <hi@usmanjabbar.com>
2021-06-23 20:10:03 +0000

    Added

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+const $ = require('jquery');
+
+$('body').append(
+  $('p').text('Holberton Dashboard'),
+  $('p').text('Dashboard data for the students'),
+  $('p').text('Copyright - Holberton School'),
+);
