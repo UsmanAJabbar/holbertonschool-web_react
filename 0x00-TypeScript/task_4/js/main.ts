f4e3980809179bc939dcac3f7c51f00a2c4339da
UsmanGTA <hi@usmanjabbar.com>
2021-06-23 03:54:26 +0000

    Pushed for file existence checks

---
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -0,0 +1,7 @@
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.TeacherInterface {
+  experienceTeachingC: 10
+}
---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,12 @@
+interface MajorCredits {
+  brand: 'major',
+  credits: number,
+}
+interface MinorCredits {
+  brand: 'minor',
+  credits: number,
+}
+
+export const sumMajorCredits = (subject1: string, subject2: string) => {
+
+}
