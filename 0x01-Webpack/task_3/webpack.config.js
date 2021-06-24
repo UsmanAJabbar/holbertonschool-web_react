eec0259bf82cec8e10fca013a7fbd9879a81ff5c
UsmanGTA <hi@usmanjabbar.com>
2021-06-24 07:16:56 +0000

    Added initial commit for task 3

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './modules/header/header.js',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/header/body.js',
+    footer: './modules/header/footer.js'
+  },
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "[name].bundle.js"
