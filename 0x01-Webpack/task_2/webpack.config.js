5f81593de6572c05e4776b6b632fb16e1de4a4aa
UsmanGTA <hi@usmanjabbar.com>
2021-06-24 05:04:12 +0000

    Added initial commit for task 2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+              test: /\.(css)$/,
+              use: ['style-loader','css-loader']
+            },
+            {
+              test: /\.(png|jp(e*)g|svg)$/,
+              use: ['file-loader']
+            }]
+  }
+}
