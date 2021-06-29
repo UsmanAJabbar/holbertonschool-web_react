fa4c136a2566d271828ad569c0d61f19a63ea51b
UsmanGTA <hi@usmanjabbar.com>
2021-06-29 07:56:34 +0000

    Added initial commit for task 3

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import './Notifications.css';
+import { getLatestNotifcation } from './utils';
+import closeButton from './close-button.png';
 
 const Notification = () => <div className="Notifications">
+                             <button aria-label="Close"
+                                     onClick={() => console.log('Close button has been clicked')}
+                                     style={ { 'display': 'inline-block',
+                                               'float': 'right',
+                                               'background-color': 'transparent',
+                                               'padding': '0', 'margin': '0',
+                                               'border': 'none' } }>
+                               <img src={closeButton} alt="close button" style={ { 'height': '20px' } } />
+                             </button>
                              <p>Here is the list of notifications</p>
+                             <ul>
+                               <li data-priority="default">New course available</li>
+                               <li data-priority="urgent">New resume available</li>
+                               <li data-priority="urgent" dangerouslySetInnerHTML={ { __html: getLatestNotifcation() } }></li>
+                             </ul>
                            </div>
 export default Notification;
--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -1,3 +1,4 @@
 export const getFullYear = () => new Date().getFullYear();
 export const getFooterCopy = (isIndex) => isIndex === true ? 'Holberton School':
                                                              'Holberton School main dashboard';
+export const getLatestNotifcation = () => '<strong>Urgent requirement</strong> - complete by EOD';
