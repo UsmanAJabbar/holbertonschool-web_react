2686e891bbd6ed2196c77fda38ccd7845925c024
UsmanGTA <hi@usmanjabbar.com>
2021-06-30 00:42:06 +0000

    Added initial commit for task 8

---
+++ 0x02-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist/',
+    port: 8564,
+    hot: true,
+  },
+  plugins: [ new HtmlWebpackPlugin() ],
+  module: {
+    rules: [{
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+            },
+            {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: ['file-loader', {
+                                    loader: 'image-webpack-loader',
+                                    options: { bypassOnDebug: true,
+                                               disable: true },
+                                 }
+                 ]
+            }],
+  },
+  devtool : 'inline-source-map'
+}
