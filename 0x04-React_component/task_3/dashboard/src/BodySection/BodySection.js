847773994db93a51387ebd64170c85737385e1f1
UsmanGTA <hi@usmanjabbar.com>
2021-07-12 03:18:07 +0000

    Added initial commit for task 3

---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class BodySection extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { title, children } = this.props;
+    <div className="bodySection">
+      <h2>{title}</h2>
+      { children }
+    </div>
+  }
+}
+
+export default BodySection;
