099707be3ec83c4495e54bcffaa937c3b3a6ea67
UsmanGTA <hi@usmanjabbar.com>
2021-07-15 15:06:52 +0000

    Added README.md

--- 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -17,7 +17,7 @@ class Notification extends React.Component {
   markAsRead(id) {
     console.log(`Notification ${id} has been marked as read`);
   }
-
+  
   shouldComponentUpdate(nextProps) {
     return nextProps.listNotifications.length > this.props.listNotifications.length
   }
