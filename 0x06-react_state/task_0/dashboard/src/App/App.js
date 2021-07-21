5cac825f012e8d4b8d94e57bb2d89d471c712d33
UsmanGTA <hi@usmanjabbar.com>
2021-07-21 19:05:20 +0000

    Added

--- 0x06-react_state/task_0/dashboard/src/App/App.js
+++ 0x06-react_state/task_0/dashboard/src/App/App.js
@@ -52,10 +52,11 @@ class App extends React.Component {
 
   render() {
     const { isLoggedIn } = this.props;
+    const { displayDrawer } = this.state;
     return (
       <React.Fragment>
         <Notifications listNotifications={ listNotifications }
-                       displayDrawer={this.state.displayDrawer}
+                       displayDrawer={displayDrawer}
                        handleDisplayDrawer={this.handleDisplayDrawer}
                        handleHideDrawer={this.handleHideDrawer}
         />
