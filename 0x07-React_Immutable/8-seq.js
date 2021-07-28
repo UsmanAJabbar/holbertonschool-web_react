7825d2aa3e7354004876c0e5acdf8c0770b257d1
UsmanGTA <hi@usmanjabbar.com>
2021-07-28 18:28:16 +0000

    Added eslint fixes

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,11 +1,12 @@
-import { Seq, Map } from 'immutable';
+import { Seq } from 'immutable';
 
 const printBestStudents = (studentsObj) => {
   const filteredStudents = Seq(studentsObj)
     .filter((student) => student.score >= 70)
-    .map(student => {
-      student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1)
-      student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
+    .map((studentObj) => {
+      const student = studentObj;
+      student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1);
+      student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1);
       return student;
     }).toObject();
 
