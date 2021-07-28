315a446eb3ffa36bee9a7a0cd59d30a9d47d7d86
UsmanGTA <hi@usmanjabbar.com>
2021-07-28 18:24:07 +0000

    New fixes

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,26 +1,25 @@
 import { Seq, Map } from 'immutable';
 
 const printBestStudents = (studentsObj) => {
-  const filteredStudents = Seq(Map(studentsObj))
+  const filteredStudents = Seq(studentsObj)
     .filter((student) => student.score >= 70)
-    .toJS();
-
-  for (const student of Object.values(filteredStudents)) {
-    student.firstName = student.firstName[0].toUpperCase() + student.lastName.slice(1)
-    student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
-  }
+    .map(student => {
+      student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1)
+      student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
+      return student;
+    }).toObject();
 
   console.log(filteredStudents);
 };
 
 export default printBestStudents;
 
-const grades = {
-  1: {
-    score: 99,
-    firstName: 'guillaume',
-    lastName: 'salva',
-  }
-};
+// const grades = {
+//   1: {
+//     score: 99,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   }
+// };
 
-printBestStudents(grades)
+// printBestStudents(grades)
