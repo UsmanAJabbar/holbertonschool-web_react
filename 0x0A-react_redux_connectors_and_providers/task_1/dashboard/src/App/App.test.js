a0e6eb33f933c8cc68d200243df7f7b3f89bb483
UsmanGTA <hi@usmanjabbar.com>
2021-08-17 14:39:53 +0500

    Updated app.test.js for task 6

--- 0x0A-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -41,35 +41,6 @@ describe('Test App component', () => {
     assert.equal(wOArgsApp.find('Login').exists(), true);
     assert.equal(wOArgsApp.find('CourseList').exists(), false);
   });
-  // it('Tests whether the <CourseList /> is rendered rather than <Login />', () => {
-  //   assert.equal(wArgsApp.find('Login').exists(), false);
-  //   assert.equal(wArgsApp.find('CourseList').exists(), true);
-  // });
-
-  // it('Tests whether alert was called when ctrl-h is pressed', () => {
-  //   const logOut = jest.fn(() => {});
- 
-  //   const alert = jest.spyOn(global, 'alert');
-
-  //   const wrapper = shallow(<App logOut={logOut} />);
-  //   wrapper.find(document)
-  //     .simulate('keydown', { key: 'h', ctrlKey: true })
-
-  //   expect(logOut).toHaveBeenCalled();
-  //   expect(alert).toHaveBeenCalled();
-
-  //   alert.mockRestore();
-  // });
-  it('Tests whether the default state for the displayDrawer Prop is false', () => {
-    assert.equal(wOArgsApp.state('displayDrawer'), false);
-  });
-  it('Tests whether both of the handler methods that update displayDrawer\'s state are working', () => {
-    const instance = wOArgsApp.instance();
-    instance.handleDisplayDrawer();
-    assert.equal(wOArgsApp.state('displayDrawer'), true);
-    instance.handleHideDrawer();
-    assert.equal(wOArgsApp.state('displayDrawer'), false);
-  });
   it('Tests whether fromJS returns the right object', () => {
     let state = fromJS({
       isUserLoggedIn: true,
